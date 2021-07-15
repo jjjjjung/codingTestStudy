8bd2b97014fe8d051fb52d8370e088e8697cf948
정현정 <p001215@naver.com>
2021-07-16 01:25:20 +0900

    Queue2

---
+++ Week2/Queue2.py
@@ -0,0 +1,17 @@
+#1158번
+n,k = map(int,input().split())
+
+arr = []
+num = 0
+ans = []
+
+for i in range(n):
+    arr.append(i+1)
+
+for i in range(n):
+    num += k-1
+    if num >= len(arr):
+        num = num % len(arr)
+    ans.append(arr.pop(num))
+
+print("<" , ", ".join(str(i) for i in ans), ">", sep='')
